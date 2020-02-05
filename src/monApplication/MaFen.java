d4638163a32dfb062ba3f5b986a7380092a6cb67
arodde <roddealain@gmail.com>
2020-02-05 13:06:03 +0100

    interface graphique commencée

--- src/monApplication/MaFen.java
+++ src/monApplication/MaFen.java
@@ -1,11 +1,59 @@
+package monApplication;
+
+import uti.Uti;
+
 import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class MaFen extends JFrame {
 
+    public JPanel mainPanel = new JPanel();
+    public JPanel interact = new JPanel();
+    public JPanel result = new JPanel();
+    public JEditorPane jEditorPaneCity = new JEditorPane();
+    public JButton jbuttonAskWeatherInformations = new JButton("Demander informations météo");
     public static void main(String[] args){
-        System.out.println("coucou");
+        MaFen mafen = new MaFen();
     }
     public MaFen(){
+        Uti.info("MaFen","MaFen()","");
 
+        this.setTitle("Affichage Météo par ville");
+        this.setSize(600, 600);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationRelativeTo(null);
+
+// add borderLayout in mainPanel
+        BorderLayout borderLayout = new BorderLayout();
+        mainPanel.setBackground(Color.white);
+        mainPanel.add(interact,BorderLayout.NORTH);
+        mainPanel.add(new JScrollPane(result),BorderLayout.CENTER);
+        result.setPreferredSize(new Dimension(this.getWidth(), this.getHeight()-50));
+        result.setBackground(Color.red);
+        interact.setPreferredSize(new Dimension(this.getWidth(), 50));
+        interact.add(jEditorPaneCity,BorderLayout.EAST);
+        interact.setBackground(Color.gray);
+        interact.add(jbuttonAskWeatherInformations,BorderLayout.WEST);
+
+        // display the window
+        this.setContentPane(mainPanel);
+        this.setVisible(true);
+    }
+    class AskApi implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            // check the text in jEditorPaneCity
+            // if it's different of "" or null
+            //      check internet connection
+            //      if connection ok
+            //          create thread
+            //          stock recept data
+            //      else
+            //          display message "Une connexion internet est requise"
+        }
     }
 }
