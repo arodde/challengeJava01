9d4221c7ca93c1f969f5cb8068d5890312f9bc34
arodde <roddealain@gmail.com>
2020-06-08 10:29:53 +0200

    ajout classe pour exploiter réponse api

--- src/monApplication/classApi/City.java
+++ src/monApplication/classApi/City.java
@@ -1,8 +1,9 @@
-package monApplication;
+package monApplication.classApi;
 public class City {
-    String insee	;// Code postal de la commune
-    String name	; //	Nom de la commune
-    float latitude;	 //	Latitude décimale de la commune
-    float longitude;	//	Longitude décimale de la commune
-    int altitude	; //	Altitude de la commune en mètres
+    public  String insee	;// Code insee de la commune
+	public  int cp ; // Code postal de la commune
+    public   String name	; //	Nom de la commune
+    public   float latitude;	 //	Latitude décimale de la commune
+    public  float longitude;	//	Longitude décimale de la commune
+    public   int altitude	; //	Altitude de la commune en mètres
 }
--- src/monApplication/classApi/Forecast_day.java
+++ src/monApplication/classApi/Forecast_day.java
@@ -1,25 +1,24 @@
-package monApplication;
-import java.time.LocalDateTime;
+package monApplication.classApi;
 public class Forecast_day {
-    String  insee;//	Code Insee de la commune
-    float  latitude;//		Latitude décimale de la commune
-    float   longitude	;//		Longitude décimale de la commune
-    Integer  day	;//		Jour entre 0 et 13 (Pour le jour même : 0, pour le lendemain : 1, etc.)
-    DateTime	datetime ;//		Date en heure locale, format ISO8601
-    Integer wind10m	;//		Vent moyen à 10 mètres en km/h
-    Integer   gust10m	 ;//		Rafales de vent à 10 mètres en km/h
-    Integer  dirwind10m	;//		Direction du vent en degrés (0 à 360°)
-    float  rr10	;//		Cumul de pluie sur la journée en mm
-    float	rr1	 ;//	Cumul de pluie maximal sur la journée en mm
-    Integer	probarain	 ;//	Probabilité de pluie entre 0 et 100%
-    Integer  weather;//		Temps sensible (Code temps) – Voir Annexes
-    Integer tmin	;//		Température minimale à 2 mètres en °C
-    Integer   tmax	;//		Température maximale à 2 mètres en °C
-    Integer sunHours	;//	Ensoleillement en heures
-    float	 etp	;// Cumul 	d'évapotranspiration en mm
-    Integer	 probafrost	 ;//	Probabilité de gel entre 0 et 100%
-    	Integer probafog;//		Probabilité de brouillard entre 0 et 100%
-   	Integer	  probawind70;//	Probabilité de vent >70 km/h entre 0 et 100%
-    	Integer probawind100;//		Probabilité de vent >100 km/h entre 0 et 100%
-    Integer gustx	;//		Rafale de vent potentielle sous orage ou grain en km/h
+//    String  insee;//	Code Insee de la commune
+//    float  latitude;//		Latitude décimale de la commune
+//    float   longitude	;//		Longitude décimale de la commune
+//    Integer  day	;//		Jour entre 0 et 13 (Pour le jour même : 0, pour le lendemain : 1, etc.)
+//    DateTime	datetime ;//		Date en heure locale, format ISO8601
+//    Integer wind10m	;//		Vent moyen à 10 mètres en km/h
+//    Integer   gust10m	 ;//		Rafales de vent à 10 mètres en km/h
+//    Integer  dirwind10m	;//		Direction du vent en degrés (0 à 360°)
+//    float  rr10	;//		Cumul de pluie sur la journée en mm
+//    float	rr1	 ;//	Cumul de pluie maximal sur la journée en mm
+//    Integer	probarain	 ;//	Probabilité de pluie entre 0 et 100%
+//    Integer  weather;//		Temps sensible (Code temps) – Voir Annexes
+//    Integer tmin	;//		Température minimale à 2 mètres en °C
+//    Integer   tmax	;//		Température maximale à 2 mètres en °C
+//    Integer sunHours	;//	Ensoleillement en heures
+//    float	 etp	;// Cumul 	d'évapotranspiration en mm
+//    Integer	 probafrost	 ;//	Probabilité de gel entre 0 et 100%
+//    	Integer probafog;//		Probabilité de brouillard entre 0 et 100%
+//   	Integer	  probawind70;//	Probabilité de vent >70 km/h entre 0 et 100%
+//    	Integer probawind100;//		Probabilité de vent >100 km/h entre 0 et 100%
+//    Integer gustx	;//		Rafale de vent potentielle sous orage ou grain en km/h
 }
